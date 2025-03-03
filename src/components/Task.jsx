5eae68e90410fe3150cfb6f158dbe5425439a79e
Mizanur Rahman Sifat <mrahmansifat98@gmail.com>
2025-03-03 10:25:07 +0600

    Third commit

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,9 +1,24 @@
-export default function Task({task}) {
+import { useState } from "react"
+
+export default function Task({ task }) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const taskContent = isEditing ? (
+        <>
+            <input type="text" />
+            <button onClick={() => setIsEditing(false)}>Save</button>
+        </>
+    ) : (
+        <>
+            {task.text}
+            <button onClick={() => setIsEditing(true)}>Edit</button>
+        </>
+    );
+
     return (
         <>
             <input type="checkbox" />
-            {task.text}
-            <button>Edit</button>
+            {taskContent}
             <button>Delete</button>
         </>
     )
